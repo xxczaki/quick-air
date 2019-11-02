a96ea6f2a9c560827573383cc5d51a3e7d5e5b0d
Antoni Kepinski <xxczaki@pm.me>
2019-11-02 20:30:54 +0100

    Add color theme switch

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import {Heading, Flex, IconButton, useColorMode} from '@chakra-ui/core';
 
 import Container from '../components/container';
-import Header from '../components/header';
 import Utility from '../components/utility';
 import Description from '../components/description';
 import Attribution from '../components/attribution';
 
 const Index = () => {
+	const {colorMode, toggleColorMode} = useColorMode();
+
 	return (
 		<Container>
-			<Header>Quick Air</Header>
+			<Flex direction="row" wrap="wrap">
+				<Heading style={{paddingRight: '0.5em'}} as="h1">Quick Air</Heading>
+				<IconButton
+					style={{width: '2em'}}
+					variant="ghost"
+					size="lg"
+					aria-label={`Toggle ${colorMode === 'light' ? 'Dark' : 'Light'}`}
+					icon={colorMode === 'light' ? 'moon' : 'sun'}
+					onClick={toggleColorMode}
+				/>
+			</Flex>
+			<br/>
 			<Description>Check air quality near you quickly!</Description>
+			<br/>
 			<Attribution/>
 			<Utility/>
 		</Container>
