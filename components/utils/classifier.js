59895d44ed9dcaa5de7c076fef6a6a2894a1bd3b
Antoni Kepinski <xxczaki@pm.me>
2019-11-03 22:45:38 +0100

    fix text & background color contrast

--- components/utils/classifier.js
+++ components/utils/classifier.js
@@ -4,34 +4,34 @@ const classifyAirQuality = index => {
 	const {level, advice} = index;
 
 	if (level === 'VERY_LOW') {
-		return {classification: 'VERY GOOD', advice, color: '#276749'};
+		return {classification: 'VERY GOOD', advice, backgroundColor: '#276749', textColor: '#fff'};
 	}
 
 	if (level === 'LOW') {
-		return {classification: 'GOOD', advice, color: '#48BB78'};
+		return {classification: 'GOOD', advice, backgroundColor: '#48BB78', textColor: '#000'};
 	}
 
 	if (level === 'MEDIUM') {
-		return {classification: 'MIXED', advice, color: '#F6E05E'};
+		return {classification: 'MIXED', advice, backgroundColor: '#F6E05E', textColor: '#000'};
 	}
 
 	if (level === 'HIGH') {
-		return {classification: 'BAD', advice, color: '#ED8936'};
+		return {classification: 'BAD', advice, backgroundColor: '#ED8936', textColor: '#000'};
 	}
 
 	if (level === 'VERY_HIGH') {
-		return {classification: 'VERY BAD', advice, color: '#E53E3E'};
+		return {classification: 'VERY BAD', advice, backgroundColor: '#E53E3E', textColor: '#000'};
 	}
 
 	if (level === 'EXTREME') {
-		return {classification: 'EXTREMELY BAD', advice, color: '#822727'};
+		return {classification: 'EXTREMELY BAD', advice, backgroundColor: '#822727', textColor: '#fff'};
 	}
 
 	if (level === 'AIRMAGEDDON') {
-		return {classification: 'HORRENDOUSLY BAD', advice, color: '#97266D'};
+		return {classification: 'HORRENDOUSLY BAD', advice, backgroundColor: '#97266D', textColor: '#fff'};
 	}
 
-	return {classification: 'UNKNOWN', advice: 'The air quality is unknown.', color: '#A0AEC0'};
+	return {classification: 'UNKNOWN', advice: 'The air quality is unknown.', backgroundColor: '#A0AEC0', textColor: '#000'};
 };
 
 export default classifyAirQuality;
